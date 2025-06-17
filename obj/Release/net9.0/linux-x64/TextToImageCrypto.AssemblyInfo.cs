0d9d0f97434d5611f8d7532aa623abbb9d13007f
Nana Sin <anime.cat1444@gmail.com>
2025-06-17 20:24:01 +0500

    Обновление .gitignore и прекращение отслеживания obj/

--- obj/Release/net9.0/linux-x64/TextToImageCrypto.AssemblyInfo.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a16f1b0f77bd514d98b82bdd8a6c6c0b025a22001f4e2444f4103b17704f7218
-size 964
--- obj/Release/net9.0/win-x64/.NETCoreApp,Version=v9.0.AssemblyAttributes.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0be1a8399fbefa553e3202c44c7fb653cf774b73b6633960d3f702df47a494b2
-size 194
